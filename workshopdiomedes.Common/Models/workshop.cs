09139a90e534a53b0bef5da043c48b3803193280
DIOMEDES DE JESUS GUERRA FELIZZOLA <diomedesguerra256177@correo.itm.edu.co>
2021-08-27 12:35:06 -0500

    add the entities

---
+++ workshopdiomedes.Common/Models/workshop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workshopdiomedes.Common.Models
+{
+    public class workshop
+    {
+        public int idemployee { get; set; }
+        public DateTime date { get; set; }
+        public int type { get; set; }
+        public Boolean consolidated { get; set; }
+    }
+}
---
+++ workshopdiomedes.Functions/Entities/WorkshopEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace workshopdiomedes.Functions.Entities
+{
+    internal class WorkshopEntity : TableEntity
+    {
+        public int idemployee { get; set; }
+        public DateTime date { get; set; }
+        public int type { get; set; }
+        public bool consolidated { get; set; }
+    }
+}
